38e279779cede346ea7177c38f110bf8541d3db7
Omer.R. <omer.rots@gmail.com>
2025-02-11 12:15:03 +0200

    first version of book.service.js

--- services/book.service.js
+++ services/book.service.js
@@ -1,8 +1,9 @@
-import { loadFromStorage, makeId, saveToStorage } from "./util.service.js";
+import { loadFromStorage, saveToStorage } from "./util.service.js";
 import { storageService } from "./async-storage.service.js";
+import { booksDB } from "../assets/booksDb.js";
 
-const CAR_KEY = "booksDB";
-_createCars();
+const BOOK_KEY = "bookDB";
+_createBooks();
 
 export const booksService = {
 	query,
@@ -10,72 +11,54 @@ export const booksService = {
 	remove,
 	save,
 	getEmptyCar,
-	getDefaultFilter,
 };
 
 function query(filterBy = {}) {
-	return storageService.query(CAR_KEY).then(cars => {
-		if (filterBy.txt) {
-			const regExp = new RegExp(filterBy.txt, "i");
-			cars = cars.filter(car => regExp.test(car.vendor));
-		}
-		if (filterBy.minSpeed) {
-			cars = cars.filter(car => car.speed >= filterBy.minSpeed);
-		}
-		return cars;
+	return storageService.query(BOOK_KEY).then(books => {
+		// if (filterBy.txt) {
+		// 	const regExp = new RegExp(filterBy.txt, "i");
+		// 	books = books.filter(car => regExp.test(car.vendor));
+		// }
+		// if (filterBy.minSpeed) {
+		// 	books = books.filter(car => car.speed >= filterBy.minSpeed);
+		// }
+		return books;
 	});
 }
 
-function get(carId) {
-	return storageService.get(CAR_KEY, carId).then(_setNextPrevCarId);
+function get(bookId) {
+	return storageService.get(BOOK_KEY, bookId).then(_setNextPrevCarId);
 }
 
-function remove(carId) {
-	return storageService.remove(CAR_KEY, carId);
+function remove(bookId) {
+	return storageService.remove(BOOK_KEY, bookId);
 }
 
-function save(car) {
-	if (car.id) {
-		return storageService.put(CAR_KEY, car);
+function save(book) {
+	if (book.id) {
+		return storageService.put(BOOK_KEY, book);
 	} else {
-		return storageService.post(CAR_KEY, car);
+		return storageService.post(BOOK_KEY, book);
 	}
 }
 
-function getEmptyCar(vendor = "", speed = "") {
-	return { vendor, speed };
-}
-
-function getDefaultFilter() {
-	return { txt: "", minSpeed: "" };
-}
-
-function _setNextPrevCarId(car) {
-	return query().then(cars => {
-		const carIdx = cars.findIndex(currCar => currCar.id === car.id);
-		const nextCar = cars[carIdx + 1] ? cars[carIdx + 1] : cars[0];
-		const prevCar = cars[carIdx - 1] ? cars[carIdx - 1] : cars[cars.length - 1];
-		car.nextCarId = nextCar.id;
-		car.prevCarId = prevCar.id;
-		return car;
+function _setNextPrevCarId(book) {
+	return query().then(books => {
+		const bookIdx = books.findIndex(currBook => currBook.id === book.id);
+		const nextBook = books[bookIdx + 1] ? books[bookIdx + 1] : books[0];
+		const prevBook = books[bookIdx - 1]
+			? books[bookIdx - 1]
+			: books[books.length - 1];
+		book.nextBookrId = nextBook.id;
+		book.prevBookId = prevBook.id;
+		return book;
 	});
 }
 
-function _createCars() {
-	let cars = loadFromStorage(CAR_KEY);
-	if (!cars || !cars.length) {
-		cars = [
-			_createCar("audu", 300),
-			_createCar("fiak", 120),
-			_createCar("subali", 50),
-			_createCar("mitsu", 150),
-		];
-		saveToStorage(CAR_KEY, cars);
+function _createBooks() {
+	let books = loadFromStorage(BOOK_KEY);
+	if (!books || !books.length) {
+		books = [...booksDB];
+		saveToStorage(BOOK_KEY, books);
 	}
-}
-
-function _createCar(vendor, speed = 250) {
-	const car = getEmptyCar(vendor, speed);
-	car.id = makeId();
-	return car;
 }
